7efb4d69b5641eef498de58c29e920a5a3d8e08e
somrita-banerjee <somritabanerjee126@gmail.com>
2024-08-24 17:59:16 +0530

    Added prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,53 +1,53 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "15px",
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
-    },
-    fontFamily: {
-      primary: "var(--font-jetbrainsMono)",
-    },
-    extend: {
-      colors: {
-        primary: '#1c1c22',
-        accent: {
-          DEFAULT: '#00ff99',
-          hover: '#00e187',
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+    ],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "15px",
         },
-      } ,
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "960px",
+            xl: "1200px",
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        fontFamily: {
+            primary: "var(--font-jetbrainsMono)",
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+        extend: {
+            colors: {
+                primary: "#1c1c22",
+                accent: {
+                    DEFAULT: "#00ff99",
+                    hover: "#00e187",
+                },
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+    plugins: [require("tailwindcss-animate")],
+} satisfies Config;
 
-export default config
+export default config;
