4e444541d50c30816740ca5f1eaba3122845aabf
somrita-banerjee <somritabanerjee126@gmail.com>
2024-08-25 10:43:52 +0530

    Update Social.tsx

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+    FaFacebook,
+    FaGithub,
+    FaInstagram,
+    FaLinkedinIn,
+    FaTwitter,
+    FaYoutube,
+} from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedinIn />, path: "" },
-    { icon: <FaYoutube />, path: "" },
-    { icon: <FaTwitter />, path: "" },
+    { icon: <FaGithub />, path: "https://github.com/somrita-banerjee" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/somrita-banerjee/" },
+    { icon: <FaInstagram />, path: "https://www.instagram.com/s_om_rita/" },
+    { icon: <FaFacebook />, path: "https://www.facebook.com/somrita.banerjee.545/" },
+    { icon: <FaTwitter />, path: "https://x.com/SomritaBan22742" },
 ];
 const Social = ({ containerStyles, iconstyles }: any) => {
     return (
