dd424bde3072aad88bf9c7a33c11c15fc9ed179d
somrita-banerjee <somritabanerjee126@gmail.com>
2024-08-25 14:14:52 +0530

    service page updated

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,89 @@
-const Services = () => {
-  return (
-    <div> services page</div>
-  )
-}
+"use client";
 
-export default Services
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const services = [
+    {
+        num: "01",
+        title: "Web Devolopment",
+        description: "lorem sag,md ",
+        href: "",
+    },
+    {
+        num: "02",
+        title: "Frontend Devolopment",
+        description: "lorem sag,md ",
+        href: "",
+    },
+    {
+        num: "01",
+        title: "Backend Devolopment",
+        description: "lorem sag,md ",
+        href: "",
+    },
+    {
+        num: "01",
+        title: "Android Devolopment",
+        description: "lorem sag,md ",
+        href: "",
+    },
+    {
+        num: "01",
+        title: "DataBase Management",
+        description: "lorem sag,md ",
+        href: "",
+    },
+    {
+        num: "01",
+        title: "UI/UX Desighn",
+        description: "lorem sag,md ",
+        href: "",
+    },
+];
+
+const Services = () => {
+    return (
+        <section className="flex min-h-[80vh] flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+                    }}
+                    className="grid grid-cols-1 gap-[60px] md:grid-cols-2"
+                >
+                    {services.map((service, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="group flex flex-1 flex-col justify-center gap-6"
+                            >
+                                <div className="flex w-full items-center justify-between">
+                                    <div className="text-outline group-hover:text-outline-hover text-5xl font-extrabold text-transparent transition-all duration-500">
+                                        {service.num}
+                                    </div>
+                                    <Link
+                                        href={service.href}
+                                        className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white transition-all duration-500 hover:-rotate-45 group-hover:bg-accent"
+                                    >
+                                        <BsArrowDownRight className="text-3xl text-primary" />
+                                    </Link>
+                                </div>
+                                <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500 group-hover:text-accent">
+                                    {service.title}
+                                </h2>
+                                <p className="text-white/60">{service.description}</p>
+                                <div className="w-full border-b border-white/20"></div>
+                            </div>
+                        );
+                    })}
+                </motion.div>
+            </div>
+        </section>
+    );
+};
+
+export default Services;
